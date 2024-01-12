f3e716c03de43dd2803d962ea68bca0d8ce34632
soonseong.k <88172590+cgtg@users.noreply.github.com>
2024-01-12 09:19:11 +0900

    [level Lv.0] Title: 나이 출력, Time: 0.14 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120820. 나이 출력/나이 출력.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int age) {
+        if( age <= 0 || age > 120 ){
+            throw new ArgumentOutOfRangeException("num1 또는 num2가 제한사항을 벗어났습니다.");
+        }
+        return 2023 - age;
+    }
+}
