e1a16b352c52ab2c92faf9d87da9aa0e59477619
soonseong.k <88172590+cgtg@users.noreply.github.com>
2024-01-12 09:26:15 +0900

    [level Lv.0] Title: 두 수의 합, Time: 0.24 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120802. 두 수의 합/두 수의 합.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        if (num1 < -50000 || num1 > 50000 || num2 < -50000 || num2 > 50000)
+        {
+            throw new ArgumentOutOfRangeException("num1 또는 num2가 제한사항을 벗어났습니다.");
+        }
+        return num1+num2;
+    }
+}
