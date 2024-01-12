5478da121ddbe584da72741b50a1e803cf59979d
soonseong.k <88172590+cgtg@users.noreply.github.com>
2024-01-12 09:14:48 +0900

    [level Lv.0] Title: 두 수의 곱, Time: 0.16 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.0/120804. 두 수의 곱/두 수의 곱.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        if( num1 < 0 || num1 > 100 || num2 <0 || num2>100 ){
+            throw new ArgumentOutOfRangeException("num1 또는 num2가 제한사항을 벗어났습니다.");
+        }
+        return num1*num2;
+    }
+}
