15e31e4e6db4f2bb255708702a0532c3e34cb0c0
soonseong.k <88172590+cgtg@users.noreply.github.com>
2024-01-16 09:10:35 +0900

    [level Lv.1] Title: 정수 제곱근 판별, Time: 0.23 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/Lv.1/12934. 정수 제곱근 판별/정수 제곱근 판별.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public long solution(long n) {
+        long sqrt = (long)Math.Sqrt(n);
+        if(sqrt*sqrt==n){
+            return (sqrt+1)*(sqrt+1);
+        }
+        else{
+            return -1;
+        }
+    }
+}
