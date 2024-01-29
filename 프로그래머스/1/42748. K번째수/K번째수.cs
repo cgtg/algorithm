37711b1ce56ba79c02b728b1ff3e440d31c6ac8f
soonseong.k <88172590+cgtg@users.noreply.github.com>
2024-01-29 09:32:08 +0900

    [level 1] Title: K번째수, Time: 2.43 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cs
+++ 프로그래머스/1/42748. K번째수/K번째수.cs
@@ -3,15 +3,15 @@ using System.Linq;
 public class Solution {
     public int[] solution(int[] array, int[,] commands) {
         int[] answer = new int[commands.GetLength(0)];
-        for (int i = 0; i < commands.GetLength(0); i++)
+        for (int l = 0; l < commands.GetLength(0); l++)
         {
-            int start = commands[i, 0] - 1;
-            int end = commands[i, 1];
-            int j = commands[i, 2] - 1;
+            int i = commands[l, 0] - 1;
+            int j = commands[l, 1];
+            int k = commands[l, 2] - 1;
 
-            int[] tmpArr = array.Skip(start).Take(end - start).ToArray();
+            int[] tmpArr = array.Skip(i).Take(j - i).ToArray();
             Array.Sort(tmpArr);
-            answer[i] = tmpArr[j];
+            answer[l] = tmpArr[k];
         }
         return answer;
     }
--- 프로그래머스/1/42748. K번째수/README.md
+++ 프로그래머스/1/42748. K번째수/README.md
@@ -1,10 +1,10 @@
 # [level 1] K번째수 - 42748 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42748?language=csharp) 
 
 ### 성능 요약
 
-메모리: 31.4 MB, 시간: 2.60 ms
+메모리: 31.5 MB, 시간: 2.43 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 9:28:52
+2024년 1월 1일 9:32:7
 
 ### 문제 설명
 
