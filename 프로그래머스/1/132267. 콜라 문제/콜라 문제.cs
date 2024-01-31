c88cc1eecd46ccff1ab63fb6cb717158896a1e80
soonseong.k <88172590+cgtg@users.noreply.github.com>
2024-01-31 09:23:06 +0900

    [level 1] Title: 콜라 문제, Time: 0.18 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int solution(int a, int b, int n) {
+        int answer = 0;
+        while(n >= a){
+            int newCoke = n / a * b;
+            answer += newCoke;
+            n = n % a + newCoke;
+        }
+        return answer;
+    }
+}
