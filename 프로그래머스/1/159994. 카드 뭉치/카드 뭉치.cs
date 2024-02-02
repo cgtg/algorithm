f5e502c32e0f61cd64491548a4d5416f40c14d9d
soonseong.k <88172590+cgtg@users.noreply.github.com>
2024-02-02 09:22:38 +0900

    [level 1] Title: 카드 뭉치, Time: 0.20 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cs
@@ -0,0 +1,22 @@
+using System;
+public class Solution {
+    public string solution(string[] cards1, string[] cards2, string[] goal) {
+        int index1 = 0, index2 = 0;
+        foreach (string word in goal)
+        {
+            if (index1 < cards1.Length && cards1[index1] == word)
+            {
+                index1++;
+            }
+            else if (index2 < cards2.Length && cards2[index2] == word)
+            {
+                index2++;
+            }
+            else
+            {
+                return "No";
+            }
+        }
+        return "Yes";
+    }
+}
